49812a68752f1b8424e281a7c1efb4274d53f5dc
VIACHESLAV BROIT <triori@gmail.com>
2026-03-02 15:12:27 -0500

    upd: check that string is not empty

--- 02_03_hash_tag.py
+++ 02_03_hash_tag.py
@@ -4,11 +4,14 @@ print("Welcome to the hashtag generator!")
 print("Please enter a phrase and we will convert it into a hashtag for you.")
 potential_hashtag = input("Enter a phrase to make hashtag: ")
 
-# remove punctuation
-potential_hashtag_with_no_punctuation = ''.join(char for char in potential_hashtag if char not in string.punctuation)
-# split the phrase into words, capitalize each word and join them together
-hashtag = '#' + ''.join(word.capitalize() for word in potential_hashtag_with_no_punctuation.split())
-# cut the hashtag to 140 characters if it's too long
-hashtag = hashtag[:140]
+if not potential_hashtag:
+    print("You entered an empty string. Please enter a valid phrase.")
+else:
+    # remove punctuation
+    potential_hashtag_with_no_punctuation = ''.join(char for char in potential_hashtag if char not in string.punctuation)
+    # split the phrase into words, capitalize each word and join them together
+    hashtag = '#' + ''.join(word.capitalize() for word in potential_hashtag_with_no_punctuation.split())
+    # cut the hashtag to 140 characters if it's too long
+    hashtag = hashtag[:140]
 
-print("Your hashtag is:", hashtag)
+    print("Your hashtag is:", hashtag)
