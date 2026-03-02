97f28ebd431974d83ca3b4969456235217f607ff
VIACHESLAV BROIT <triori@gmail.com>
2026-03-02 15:04:22 -0500

    upd: var rule

--- 02_03_variable_name.py
+++ 02_03_variable_name.py
@@ -3,6 +3,7 @@ import string
 
 potential_variable_name = input("Enter a potential variable name: ")
 is_valid_variable_name = True
+unacceptable_characters = string.punctuation.replace("_", "") + string.whitespace
 
 if not potential_variable_name:
     # empty string is not a valid variable name
@@ -13,11 +14,11 @@ elif potential_variable_name[0].isdigit():
 elif any(char.isupper() for char in potential_variable_name):
     # variable name cannot contain uppercase letters
     is_valid_variable_name = False
-elif any(char in string.punctuation.replace("_", "") or char.isspace() for char in potential_variable_name):
+elif any(char in unacceptable_characters for char in potential_variable_name):
     # variable name cannot contain special characters or spaces (except for _)
     is_valid_variable_name = False
-elif any(a == "_" and b == "_" for a, b in zip(potential_variable_name, potential_variable_name[1:])):
-    # variable name cannot contain consecutive underscores (I made this rule up to match the examples provided)
+elif all(char == "_" for char in potential_variable_name) and len(potential_variable_name) > 1:
+    # variable name cannot consist entirely of more than one underscore
     is_valid_variable_name = False
 elif potential_variable_name in keyword.kwlist:
     # variable name cannot be a reserved keyword
